1154028fffe322a07203b3935a2c5b93ee3736d0
Eduardo Anabalón <lalo1412@gmail.com>
2012-01-06 14:02:33 -0300

    Ordenando código. Estructura graph. Algunas funciones

---
+++ rdf_database.h
@@ -0,0 +1,12 @@
+/*****
+** rdf_database.h.
+** contiene cabeceras de todas las funciones para crear y manejar
+** la base de datos de grafos RDF, contiene las estructuras.
+*****/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <raptor2.h>
+
+#include "utils.h"
